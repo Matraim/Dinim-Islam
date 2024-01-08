f2219e5086060e3dbd6c5adb33648f02ae1e2b85
Bismillah <cupertino@Matraims-MacBook-Pro.local>
2024-01-08 23:18:25 +0600

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 import React, { useEffect } from 'react';
-import {
-  HashRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginForm from './layout/LoginForm';
 import Quiz from './components/Quiz';
 import AdminPanel from './components/AdminPanel';
