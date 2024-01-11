895ec8fc3af28286e16536faf185b143feaadf34
Bismillah <cupertino@Matraims-MacBook-Pro.local>
2024-01-11 21:54:12 +0600

    ustazdar and 99 names of allah

---
+++ src/data/islam.js
@@ -0,0 +1,37 @@
+import Allah99Name from '../assets/images/3D-Colorful-Metal-99-Names-of-Allah-Wall-Art-Asmaul-Husna-Gold-in-Qatar-600x600.jpg';
+
+const Bismillah = [
+  {
+    name: 'Аллах Тааланын',
+    path: '/',
+    text: '99 ысым сыпаты',
+    image: Allah99Name,
+    status: 'маанилуу',
+  },
+  {
+    name: 'Аллах Тааланын',
+    path: '/',
+    text: '99 ысым сыпаты',
+    image: Allah99Name,
+  },
+  {
+    name: 'Аллах Тааланын',
+    path: '/',
+    text: '99 ысым сыпаты',
+    image: Allah99Name,
+  },
+  {
+    name: 'Аллах Тааланын',
+    path: '/',
+    text: '99 ысым сыпаты',
+    image: Allah99Name,
+  },
+  {
+    name: 'Аллах Тааланын',
+    path: '/',
+    text: '99 ысым сыпаты',
+    image: Allah99Name,
+  },
+];
+
+export default Bismillah;
