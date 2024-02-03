40c69515fec8dc6df36dbabf906d59e1a121f5bc
Bismillah <matraim.kg@gmail.com>
2024-02-04 01:56:00 +0600

    hafiz

--- src/components/Quran.jsx
+++ src/components/Quran.jsx
@@ -1,12 +1,96 @@
-import { Container } from '@mui/material';
 import React from 'react';
+import { Container, Card as MuiCard, IconButton, Rating } from '@mui/material';
+import { FavoriteBorderOutlined, FavoriteOutlined } from '@mui/icons-material';
+import styled from 'styled-components';
+import { quranData } from '../data/quranData';
 
 const Quran = () => {
+  const handleLikeClick = (id) => {
+    // Logic to handle like button click
+  };
+
+  const handleRatingChange = (id, value) => {
+    // Logic to handle rating change
+  };
+
   return (
     <Container>
       <h1>Quran</h1>
+      <div
+        style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}
+      >
+        {quranData.map((item) => (
+          <Card key={item.id}>
+            <IconContainer>
+              <IconButton
+                aria-label="like"
+                onClick={() => handleLikeClick(item.id)}
+              >
+                {item.liked ? <FavoriteOutlined /> : <FavoriteBorderOutlined />}
+              </IconButton>
+            </IconContainer>
+            <Image src={item.imageUrl} alt={item.name} />
+            <div>
+              <Title>{item.name}</Title>
+              <TextRow>
+                <Text>{item.nationality}</Text>
+                <Text>|</Text>
+                <Text>{item.position}</Text>
+              </TextRow>
+              <StyleHafizQuran
+                name={`rating-${item.id}`}
+                value={item.rating}
+                onChange={(event, newValue) =>
+                  handleRatingChange(item.id, newValue)
+                }
+              />
+            </div>
+          </Card>
+        ))}
+      </div>
     </Container>
   );
 };
 
 export default Quran;
+
+const Card = styled(MuiCard)`
+  margin: 10px;
+  position: relative;
+`;
+
+const Image = styled.img`
+  height: 200px;
+  width: auto;
+`;
+
+const Title = styled.h2`
+  font-size: 1.5rem;
+  margin-bottom: 0.5rem;
+`;
+
+const TextRow = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 0.5rem;
+`;
+
+const Text = styled.div`
+  font-size: 1rem;
+  color: #666;
+  margin-right: 5px;
+  text-align: center;
+`;
+
+const IconContainer = styled.div`
+  position: absolute;
+  top: 5px;
+  right: 5px;
+`;
+
+const StyleHafizQuran = styled(Rating)(() => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
---
+++ src/data/quranData.js
@@ -0,0 +1,41 @@
+import Hafizimg from '../assets/images/1200px-Rashidun_Caliph_Uthman_ibn_Affan_-_عثمان_بن_عفان_ثالث_الخلفاء_الراشدين.svg.png';
+
+const quranData = [
+  {
+    id: 1,
+    name: 'Мишари Рашид Аль-Афаси',
+    nationality: 'Кувейт',
+    position: 'Имам-хатыйб Соборной мечеть Кувейт,',
+    imageUrl: Hafizimg,
+  },
+  {
+    id: 2,
+    name: 'Абдул-Басит',
+    nationality: 'Каир',
+    position: 'Египетский хафиз',
+    imageUrl: Hafizimg,
+  },
+  {
+    id: 3,
+    name: 'Саад Аль-Гамиди',
+    nationality: 'Саудовской Аравии',
+    position: 'хафиз Корана',
+    imageUrl: Hafizimg,
+  },
+  {
+    id: 4,
+    name: 'Мухаммад Аюб',
+    nationality: 'Мьянма',
+    position: '1990 Имам Мечети Пророка Мухаммада сав',
+    imageUrl: Hafizimg,
+  },
+  {
+    id: 5,
+    name: 'Муъмин Айнул Мубарак',
+    nationality: 'Индонезийский',
+    position: 'хафиз',
+    imageUrl: Hafizimg,
+  },
+];
+
+export { quranData };
