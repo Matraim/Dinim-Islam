8d8bac2997d976c9c9651d8df9ba847b8230f6e9
Bismillah <matraim.kg@gmail.com>
2024-03-06 17:56:16 +0600

    random-textt

--- src/components/Naziat.jsx
+++ src/components/Naziat.jsx
@@ -1,31 +1,48 @@
 import React, { useState, useEffect } from 'react';
-import { Typography, makeStyles } from '@mui/material';
-import { randomText } from '../data/naziaat';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    margin: theme.spacing(2),
-  },
-}));
+import { randomText } from '../data/naziaat.js';
+import { styled } from '@mui/material';
 
 const Naziat = () => {
-  const classes = useStyles();
-  const [selectedText, setSelectedText] = useState('');
+  const [reminder, setReminder] = useState('');
 
   useEffect(() => {
-    const interval = setInterval(() => {
+    const setRandomReminder = () => {
       const randomIndex = Math.floor(Math.random() * randomText.length);
-      setSelectedText(randomText[randomIndex].text);
-    }, 5000);
+      setReminder(randomText[randomIndex].text);
+    };
+
+    setRandomReminder();
+
+    const interval = setInterval(() => {
+      setRandomReminder();
+    }, 9000);
 
     return () => clearInterval(interval);
   }, []);
 
   return (
-    <div className={classes.root}>
-      <Typography variant="body1">{selectedText}</Typography>
-    </div>
+    <StyleContainer>
+      <StyleRandomText>{reminder}</StyleRandomText>
+    </StyleContainer>
   );
 };
 
 export default Naziat;
+
+const StyleContainer = styled('div')(() => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  paddingBottom: '3rem',
+}));
+
+const StyleRandomText = styled('p')(() => ({
+  fontSize: '2rem',
+  textAlign: 'center',
+  backgroundColor: '#2d2d2d',
+  color: '#fff',
+  padding: '3rem',
+  borderRadius: '0.5rem',
+  fontFamily: 'monospace',
+}));
